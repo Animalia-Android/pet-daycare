0a48ebf4e1a314896c1b345b5eba7db75d9e697f
Doug Sellers <DouglasSSellers@gmail.com>
2025-07-18 16:30:52 -0400

    Added: responsive grid layout and styling

---
+++ src/components/pet-list.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+
+export default function PetList() {
+  return (
+    <ul className="bg-white border-b border-black/[0.08]">
+      <li>
+        <button className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+          <Image
+            className="rounded-full object-cover"
+            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+            alt="Pet Image"
+            width={45}
+            height={45}
+          />
+          <p className="font-semibold">Mushu</p>
+        </button>
+      </li>
+    </ul>
+  );
+}
