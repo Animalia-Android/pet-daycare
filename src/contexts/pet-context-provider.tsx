61bc79e5e7b577f9f059092251be7b49bd07285c
Doug Sellers <DouglasSSellers@gmail.com>
2025-07-18 18:02:38 -0400

    Added: PetContextProvider and Typing

---
+++ src/contexts/pet-context-provider.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { Pet } from '@/lib/types';
+import React, { createContext, useState } from 'react';
+
+type PetContextProviderProps = {
+  data: Pet[];
+  children: React.ReactNode;
+};
+
+type TPetContext = {
+  pets: Pet[];
+  selectedPetId: string | null;
+  // You can add more properties or methods as needed
+};
+
+export const PetContext = createContext<TPetContext | null>(null);
+
+export default function PetContextProvider({
+  data,
+  children,
+}: PetContextProviderProps) {
+  const [pets, setPets] = useState(data);
+  const [selectedPetId, setSelectedPetId] = useState(null);
+  return (
+    <PetContext.Provider
+      value={{
+        pets,
+        selectedPetId,
+      }}
+    >
+      {children}
+    </PetContext.Provider>
+  );
+}
