b90799a0f04f6348daed844ffed004e88495de6f
Doug Sellers <DouglasSSellers@gmail.com>
2025-07-18 17:26:58 -0400

    Fetch pets data from test API and type Pets

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'bytegrad.com' }],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bytegrad.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
 };
 
--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,20 +1,27 @@
+import { Pet } from '@/lib/types';
 import Image from 'next/image';
 
-export default function PetList() {
+type PetListProps = {
+  pets: Pet[];
+};
+
+export default function PetList({ pets }: PetListProps) {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
-      <li>
-        <button className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
-          <Image
-            className="rounded-full object-cover"
-            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
-            alt="Pet Image"
-            width={45}
-            height={45}
-          />
-          <p className="font-semibold">Mushu</p>
-        </button>
-      </li>
+      {pets.map((pet) => (
+        <li key={pet.id}>
+          <button className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+            <Image
+              className="rounded-full object-cover w-[45px] h-[45px]"
+              src={pet.imageUrl}
+              alt={`${pet.name} Image`}
+              width={45}
+              height={45}
+            />
+            <p className="font-semibold">{pet.name}</p>
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
