1c107a225b9bf2f47dc51b7d81b10948d348e00c
Doug Sellers <DouglasSSellers@gmail.com>
2025-07-26 14:38:17 -0400

    Added: default image for pet

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -13,7 +13,9 @@ export async function addPet(formData) {
       name: formData.get('name'),
       ownerName: formData.get('ownerName'),
       age: parseInt(formData.get('age')),
-      imageUrl: formData.get('imageUrl'),
+      imageUrl:
+        formData.get('imageUrl') ||
+        'https://cdn-icons-png.flaticon.com/512/235/235405.png',
       notes: formData.get('notes'),
     },
   });
