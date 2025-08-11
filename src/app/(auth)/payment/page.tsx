491cbc96a763df694ff05803e9b26842fb78d397
Doug Sellers <DouglasSSellers@gmail.com>
2025-08-11 12:05:18 -0400

    chore: added types to searchParams

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -3,18 +3,26 @@
 import { createCheckoutSession } from '@/actions/actions';
 import H1 from '@/components/h1';
 import { Button } from '@/components/ui/button';
-import React from 'react';
+import React, { useTransition } from 'react';
 
-export default function Page({ searchParams }) {
-  console.log('Search Params:', searchParams);
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const [isPending, startTransistion] = useTransition();
+
   return (
     <main className="flex flex-col items-center space-y-10">
       {!searchParams.success && (
         <>
           <H1>PetSoft access requires payment</H1>
           <Button
+            disabled={isPending}
             onClick={async () => {
-              await createCheckoutSession();
+              startTransistion(async () => {
+                await createCheckoutSession();
+              });
             }}
           >
             Buy lifetime access for $299{' '}
@@ -25,6 +33,12 @@ export default function Page({ searchParams }) {
       {searchParams.success && (
         <p className="text-lg text-green-700">
           Payment successful! You now have lifetime access to PetSoft.
+        </p>
+      )}
+
+      {searchParams.cancelled && (
+        <p className="text-lg text-red-700">
+          Payment cancelled. Please try again.
         </p>
       )}
     </main>
